0ca54d163c7af8d198f54dd526a7b94f95469ea2
Andreas Kihlberg <andreas.kihlberg@infomaker.se>
2016-11-15 13:02:30 +0100

    Added converter for strong and italic

---
+++ writer/packages/emphasis/EmphasisXMLConverter.js
@@ -0,0 +1,13 @@
+/*
+ * HTML converter for Blockquote.
+ */
+export default {
+
+    type: 'emphasis',
+    tagName: 'em',
+
+    matchElement: function(el) {
+        return el.is('em, i')
+    }
+
+}
---
+++ writer/packages/strong/StrongXMLConverter.js
@@ -0,0 +1,13 @@
+/*
+ * HTML converter for Strong.
+ */
+export default {
+
+    type: "strong",
+    tagName: "strong",
+
+    matchElement: function(el) {
+        return el.is("strong, b")
+    }
+
+}
